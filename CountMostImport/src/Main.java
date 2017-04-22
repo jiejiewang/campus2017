4201f893f3dfcfbaac1c8b5dae601b6b940aa116
jiejiewang <wang_jie_jie_nj@163.com>
2017-04-22 19:10:53 +0800

    Update Main.java

--- CountMostImport/src/Main.java
+++ CountMostImport/src/Main.java
@@ -1,102 +1,169 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.*;
-
-/**
- * Created by WangWeiyi on 2017/1/7 0007.
+/*
+三、根据指定项目目录下（可以认为是java源文件目录）中，统计被import最多的类，前十个是什么。
  */
+
 public class Main {
 
-    public static void main(String[] args){
-        String path = "";
-        System.out.println("请输入需要统计的目录");
-        Scanner in = new Scanner(System.in);
-        path = in.nextLine();
-        //path = "C:\\\\Program Files\\\\Java\\\\jdk1.8.0_45\\\\src";
+    private Map<String,Integer> importMap =new HashMap<String,Integer>();
 
-        List<File> list = new ArrayList<>();
-        Map<String, Integer> map = new HashMap<>();
-        list = getFiles(path, list);
-        for(File f : list){
-            try {
-                map = getImport(f, map);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    private class Pair implements Comparable<Pair>
+    {
+        String importName;
+        int count;
+        Pair(String name, int count)
+        {
+            importName = name;
+            this.count = count;
         }
-        map = sortImport(map);
 
-        System.out.println("统计结果如下：");
-        Iterator it = map.entrySet().iterator();
-        int count=1;
-        while(it.hasNext()){
-            Map.Entry entry = (Map.Entry) it.next();
-            System.out.println(count+":"+entry.getKey() + ":" + entry.getValue());
-            count++;
-            if(count >  10)
-                break;
+        public int compareTo(Pair o)
+        {
+            return o.count - count;
+        }
+
+        @Override
+        public String toString()
+        {
+            return '"' + importName + '"' + ": " + count;
         }
     }
-    public static Map<String, Integer> sortImport(Map<String, Integer> map){
-        ArrayList<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return o2.getValue() - o1.getValue();
-            }
-        });
-        Map<String, Integer> result = new LinkedHashMap<String, Integer>();
-        for(Map.Entry<String, Integer> entry : list){
-            result.put(entry.getKey(),entry.getValue());
+
+    /*
+     *
+     * @param dir
+     */
+    public void getFile(String dir)
+    {
+        if (dir == null)
+        {
+            return;
         }
-        return result;
+
+        File file =new File(dir);
+        if (!file.exists())
+        {
+            System.out.println("File Not Exists");
+            return;
+        }
+
+        Stack<File> stack = new Stack<File>();
+        stack.push(file);
+
+        while (!stack.isEmpty())
+        {
+            file = stack.peek();
+            stack.pop();
+
+            for(File f :file.listFiles())
+            {
+                if (f.isFile())
+                {
+                    countImport(f);
+                } else if (f.isDirectory()){
+                    stack.push(f);
+                }
+
+            }
+        }
+
     }
 
-    //读取文件中的引用信息
-    public static Map<String, Integer> getImport(File file, Map<String, Integer> map) throws IOException {
+    /*
+     *
+     * @param file
+     */
+    public void countImport(File file)
+    {
+        if (!file.getName().contains(".java"))
+        {
+            return;
+        }
         BufferedReader br = null;
-        InputStreamReader sr = null;
+
         try {
-            sr = new InputStreamReader(new FileInputStream(file));
-            br = new BufferedReader(sr);
-            String str = "";
-            while((str = br.readLine())!= null){
-                if(str.startsWith("import")){
-                    str = str.replaceAll("\\s", "").substring(6);
-                    if(!map.containsKey(str) ){
-                        map.put(str, 1);
+            br = new BufferedReader(new FileReader(file));
+            String line = null;
+            boolean isImport = false;
+
+            while ((line=br.readLine())!=null)
+            {
+                // 忽略 import static 行
+                Pattern p = Pattern.compile("^import\\s+static\\s+");
+                Matcher m = p.matcher(line.trim());
+                if (m.find()) {
+                    continue;
+                }
+
+                // 匹配 import 开始行
+                p = Pattern.compile("^import\\s+");
+                m = p.matcher(line.trim());
+                if (m.find()) {
+                    isImport = true;
+                    String temp = line.trim().substring(6).replace(";", "");
+                    if (importMap.containsKey(temp))
+                    {
+                        int count = importMap.get(temp);
+                        count++;
+                        importMap.put(temp,count);
                     } else {
-                        map.put(str, map.get(str)+1);
+                        importMap.put(temp,1);
                     }
                 }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            br.close();
-            sr.close();
-        }
-        return map;
-    }
-
-    //深度优先遍历，获取list
-    public static List<File> getFiles(String path, List<File> list){
-        File file = new File(path);
-        if(file.exists()){
-            File[] files = file.listFiles();
-            if(files.length > 0){
-                for(File f : files){
-                    if(f.isDirectory()){
-                        getFiles(f.getAbsolutePath(),list);
-                    } else {
-                        list.add(f);
-                    }
+            if (br != null)
+            {
+                try {
+                    br.close();
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
                 }
             }
-
         }
-        return list;
     }
 
+    /**
+     *
+     */
+    public void countTop10Import()
+    {
+        List<Pair> list = new ArrayList<Pair>();
+        Set<String> importNames = importMap.keySet();
+        for(String importName :importNames)
+        {
+            Pair pair = new Pair(importName,importMap.get(importName));
+            list.add(pair);
+        }
+
+        Pair[] pairArray = new Pair[0];
+        pairArray = list.toArray(pairArray);
+        Arrays.sort(pairArray);
+        int N=0;
+        for (Pair pair : pairArray)
+        {
+            if (++N >10)
+            {
+                break;
+            }
+            System.out.println(pair);
+        }
+    }
+
+    public static void main(String[] args) {
+        String dirPath = "\\Users\\Administrator\\IdeaProjects\\CountMostImport\\src";
+        Main c = new Main();
+        c.getFile(dirPath);
+        c.countTop10Import();
+    }
 }
